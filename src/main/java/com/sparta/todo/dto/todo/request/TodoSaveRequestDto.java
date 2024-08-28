648d27a96505b1c09d21b1bf2b378593798d0f44
20240729 <yoohyeonsik@gmail.com>
2024-08-28 20:55:29 +0900

    댓글 추가 완성-리퀘스트에 남은 암호랑 유저네임 삭제

--- src/main/java/com/sparta/todo/dto/todo/request/TodoSaveRequestDto.java
+++ src/main/java/com/sparta/todo/dto/todo/request/TodoSaveRequestDto.java
@@ -4,9 +4,7 @@ import lombok.Getter;
 
 @Getter
 public class TodoSaveRequestDto {
-    private String username;
     private String title;
     private String content;
-    private String password;
 }
 
